c87ecd8135329487e242e7e08525821cbcb758a3
Gowixx <57415348+Gowixx@users.noreply.github.com>
2020-09-13 00:58:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.command()
+async def tweet(ctx, username: str, *, message: str): # b'\xfc'
+    await ctx.message.delete()
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(f"https://nekobot.xyz/api/imagegen?type=tweet&username={username}&text={message}") as r:
+            res = await r.json()
+            em = discord.Embed()
+            em.set_image(url=res["message"])
+            await ctx.send(embed=em)
+
 if __name__ == "__main__":
     bot.run(TOKEN)
