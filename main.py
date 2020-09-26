ddcdf90903976b292360ea82f9d49957b3a08772
Gowixx <57415348+Gowixx@users.noreply.github.com>
2020-09-26 18:45:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,30 @@ async def config(ctx):
 async def mineplex(ctx):
     await ctx.send("`Mineplex Config`")
     
+@nuke.error
+async def kick_error(error, ctx):
+   if isinstance(error, MissingPermissions):
+       await ctx.send("You don't have permission to do that!") 
+
+@bot.command()
+@has_permissions(kick_members=True)  
+async def nuke(ctx, channel: discord.TextChannel = None):
+    channel = channel if channel else ctx.channel
+    newchannel = await channel.clone()
+    await newchannel.edit(position=channel.position)
+    await channel.delete()
+    embed = discord.Embed(
+
+        title = 'nuke go brr',
+        description = '',
+        color = discord.Color.blue( )
+    )
+    embed.set_footer(text='DortBot')
+    embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seal_of_the_Federal_Bureau_of_Investigation.svg/1200px-Seal_of_the_Federal_Bureau_of_Investigation.svg.png')
+    embed.set_author(name='NUKED CHANNEL',
+    icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seal_of_the_Federal_Bureau_of_Investigation.svg/1200px-Seal_of_the_Federal_Bureau_of_Investigation.svg.png')
+
+    await newchannel.send(embed=embed, delete_after=10)
+    
 if __name__ == "__main__":
     bot.run(TOKEN)
