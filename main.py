9b40a664e92f13b419890e17fb6a2ffa50f012f4
Gowixx <57415348+Gowixx@users.noreply.github.com>
2020-09-26 18:53:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,10 @@ async def mineplex(ctx):
    
 
 @bot.command()
-@has_permissions(Administrator=True)  
 async def nuke(ctx, channel: discord.TextChannel = None):
     channel = channel if channel else ctx.channel
     newchannel = await channel.clone()
-    if discord.User permissions = "admin":
-        try:
-          await newchannel.edit(position=channel.position)
+    await newchannel.edit(position=channel.position)
     await channel.delete()
     embed = discord.Embed(
 
@@ -32,15 +29,12 @@ async def nuke(ctx, channel: discord.TextChannel = None):
         description = '',
         color = discord.Color.blue( )
     )
-    embed.set_footer(text='DortBot')
+    embed.set_footer(text='DortWare')
     embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seal_of_the_Federal_Bureau_of_Investigation.svg/1200px-Seal_of_the_Federal_Bureau_of_Investigation.svg.png')
     embed.set_author(name='NUKED CHANNEL',
     icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seal_of_the_Federal_Bureau_of_Investigation.svg/1200px-Seal_of_the_Federal_Bureau_of_Investigation.svg.png')
 
     await newchannel.send(embed=embed, delete_after=10)
-        except Exception as e:
-          ee = "Error: " + e
-          await ctx.send_message(content=ee)
     
     
 
