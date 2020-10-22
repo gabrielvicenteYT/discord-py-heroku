67feeccb02df782d6c279cfc7cbe116aad3d55cf
Gowixx <57415348+Gowixx@users.noreply.github.com>
2020-10-22 18:46:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from discord.ext import commands
 import discord
 
-bot = commands.Bot(command_prefix="-")
+bot = commands.Bot(command_prefix="$")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
@@ -10,26 +10,23 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command()
-async def config(ctx):
-    await ctx.send("Current Configs: `-Mineplex` `-Verus` `-Ghostly`")
+async def intent(ctx):
+    await ctx.send("`Intent Download`\nhttps://intent.store/intentlauncher/Intent%20Launcher.zip")
 
-@bot.command()
-async def mineplex(ctx):
-    await ctx.send("`Mineplex Config`")
    
 
-@bot.command()
-async def nuke(ctx, channel: discord.TextChannel = None):
-    """Nuke a whole Channel"""
-    channel = channel if channel else ctx.channel
-    newchannel = await channel.clone()
-    await newchannel.edit(position=channel.position)
-    await channel.delete()
-    await newchannel.send(
-        embed=ctx.embed(
-            footer=f'💣 Channel #{channel.name} successfully nuked by '
-                   f'{ctx.author.display_name}',
-            color=0x1FFF00), delete_after=30)
+# @bot.command()
+#async def nuke(ctx, channel: discord.TextChannel = None):
+ #   """Nuke a whole Channel"""
+  #  channel = channel if channel else ctx.channel
+   # newchannel = await channel.clone()
+    #await newchannel.edit(position=channel.position)
+    #await channel.delete()
+    #await newchannel.send(
+     #   embed=ctx.embed(
+      #      footer=f'💣 Channel #{channel.name} successfully nuked by '
+       #            f'{ctx.author.display_name}',
+        #    color=0x1FFF00), delete_after=30)
     
     
 
